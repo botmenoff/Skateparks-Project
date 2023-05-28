98523218e25b05d672818caccf5ccc2447ad6083
Ferran <marvenferry60@gmail.com>
2023-05-28 21:52:59 +0200

    Models-changes

---
+++ skateparksapi/app/Http/Controllers/SkateparkController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Skatepark;
+use App\Http\Requests\StoreSkateparkRequest;
+use App\Http\Requests\UpdateSkateparkRequest;
+
+class SkateparkController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSkateparkRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Skatepark $skatepark)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Skatepark $skatepark)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSkateparkRequest $request, Skatepark $skatepark)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Skatepark $skatepark)
+    {
+        //
+    }
+}
---
+++ skateparksapi/app/Http/Controllers/SkateparksFeaturesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SkateparksFeatures;
+use App\Http\Requests\StoreSkateparksFeaturesRequest;
+use App\Http\Requests\UpdateSkateparksFeaturesRequest;
+
+class SkateparksFeaturesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreSkateparksFeaturesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(SkateparksFeatures $skateparksFeatures)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(SkateparksFeatures $skateparksFeatures)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateSkateparksFeaturesRequest $request, SkateparksFeatures $skateparksFeatures)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(SkateparksFeatures $skateparksFeatures)
+    {
+        //
+    }
+}
