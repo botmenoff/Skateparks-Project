dbf1d4e75b591ea40ecade7128cff0c30de57d6f
Ferran <marvenferry60@gmail.com>
2023-05-29 22:59:18 +0200

    Login + register and tweaking models

--- skateparksapi/app/Models/Spot.php
+++ skateparksapi/app/Models/Spot.php
@@ -8,4 +8,31 @@ use Illuminate\Database\Eloquent\Model;
 class Spot extends Model
 {
     use HasFactory;
+
+    // Tabla
+    protected $table = 'spots';
+
+    // Datos que devuelve
+    protected $fillable = [
+        'name',
+        'description',
+        'private',
+        'rating',
+        'favourite',
+        'features',
+        'location',
+        'photos',
+        'userid',
+    ];
+
+    // Relaciones
+    public function feature()
+    {
+        return $this->belongsTo(SpotFeature::class, 'features');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'userid');
+    }
 }
